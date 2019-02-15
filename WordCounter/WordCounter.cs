62061d2cc33e336db9f7e9ba9a0f6e1c78de9b8c
jaredh123 <jared.hanson12345@gmail.com>
2019-02-15 15:40:54 -0800

    Test of functions

--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -6,11 +6,17 @@ class Wordcounter
   {
     Console.WriteLine("\nEnter a sentence to look through for a word then press enter.\nNext enter the word to be searched for withing the sentence then press enter.\nThe computer will tell you how many times the word is in the sentence.\nIf your searched for word is part of another word it will not be counted.\n");
 
-    string userSentence = Console.ReadLine();
-    string userWord = Console.ReadLine();
+    string userSentence = Console.ReadLine().ToLower();
+    string userWord = Console.ReadLine().ToLower();
 
-    
-
+    if (userSentence.Contains(userWord))
+    {
+      Console.WriteLine(userSentence + ":\n" + userWord + " is in the above statement x times");
+    }
+    else
+    {
+      Console.WriteLine("false");
+    }
 
 
   }
