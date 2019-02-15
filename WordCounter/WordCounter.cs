a0f9fce4c8474aa0f08689862feead1d50e866c8
jaredh123 <jared.hanson12345@gmail.com>
2019-02-15 14:54:32 -0800

    User prompt

---
+++ WordCounter/WordCounter.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Wordcounter
+{
+  static void Main()
+  {
+    Console.WriteLine("\nEnter a sentence to look through for a word then press enter.\nNext enter the word to be searched for withing the sentence then press enter.\nThe computer will tell you how many times the word is in the sentence.\nIf your searched for word is part of another word it will not be counted.\n");
+
+    string userSentence = Console.ReadLine();
+    string userWord = Console.ReadLine();
+
+    
+
+
+
+  }
+}
